9b7079d3011793f3fd7402eda8e6507c37bd34c8
theakistea <purwadarozatunakis@gmail.com>
2021-03-24 10:15:50 +0700

    update Module

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 
-import { Province } from "./entity/Province"
-import { City } from "./entity/City";
 import { readCsvToEntity } from "./utils/readscv";
+import { BaseEntity } from "typeorm";
+import { Province } from "./entity/Province";
+import { City } from "./entity/City";
 import { District } from "./entity/District";
 
 
-const initIndoData = async () => {
-    await readCsvToEntity(__dirname + "/../csv/provinces.csv", Province)
-    await readCsvToEntity(__dirname + "/../csv/districts.csv", District)
-    await readCsvToEntity(__dirname + "/../csv/cities.csv", City)
+const initIndoData = async ( 
+    provinceModel: BaseEntity,
+    cityModel: BaseEntity,
+    districtModel: BaseEntity,) => {
+    await readCsvToEntity(__dirname + "/../csv/provinces.csv", provinceModel)
+    await readCsvToEntity(__dirname + "/../csv/districts.csv", districtModel)
+    await readCsvToEntity(__dirname + "/../csv/cities.csv", cityModel)
 
 }
 
