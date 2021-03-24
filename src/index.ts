9b7c04da40243fce8e95d1a46ee4b165097f1c28
theakistea <purwadarozatunakis@gmail.com>
2021-03-24 10:26:52 +0700

    add ts compiler

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 
 import { readCsvToEntity } from "./utils/readscv";
-import { BaseEntity } from "typeorm";
+// import { BaseEntity } from "typeorm";
 import { Province } from "./entity/Province";
 import { City } from "./entity/City";
 import { District } from "./entity/District";
 
 
 const initIndoData = async ( 
-    provinceModel: BaseEntity,
-    cityModel: BaseEntity,
-    districtModel: BaseEntity,) => {
+    provinceModel: any,
+    cityModel: any,
+    districtModel: any,) => {
     await readCsvToEntity(__dirname + "/../csv/provinces.csv", provinceModel)
     await readCsvToEntity(__dirname + "/../csv/districts.csv", districtModel)
     await readCsvToEntity(__dirname + "/../csv/cities.csv", cityModel)
